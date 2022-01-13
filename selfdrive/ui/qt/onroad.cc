9c12ce5fb15a5c06ead524c79240357becd91538
juliandoyle <97332462+juliandoyle@users.noreply.github.com>
2022-01-13 18:17:42 +1100

    Remove extra comment

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -307,7 +307,7 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIScene &scene) {
       painter.setBrush(QColor::fromRgbF(1.0, 0, 0, std::clamp<float>(1.0 - scene.road_edge_stds[i], 0.0, 1.0)));
       painter.drawPolygon(scene.road_edge_vertices[i].v, scene.road_edge_vertices[i].cnt);
     }
-#  }
+  }
   // paint path
   QLinearGradient bg(0, height(), 0, height() / 4);
   bg.setColorAt(0, scene.end_to_end ? redColor() : QColor(255, 255, 255));
